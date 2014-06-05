fe448395eb7e344d42053303f2379ca3aa7b469a
Kris De Volder <kdevolder@gopivotal.com>
2014-06-05 09:16:32 -0700

    First steps with oauth on the web client side.

--- node.server/startup-all-in-one.js
+++ node.server/startup-all-in-one.js
@@ -12,14 +12,108 @@
 /*global require console exports process __dirname*/
 
 // create and configure express
+var URI = require('URIjs');
 var express = require('express');
 var mongo = require('mongodb');
 var app = express();
-
-app.use("/client", express.static(__dirname + '/web-editor'));
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var githubSecret = require('./github-secret');
 
 var host = process.env.VCAP_APP_HOST || 'localhost';
 var port = process.env.VCAP_APP_PORT || '3000';
+var homepage = '/client/html/index.html';
+var pathResolve = require('path').resolve;
+
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: githubSecret.id,
+    clientSecret: githubSecret.secret,
+    callbackURL: "http://"+host+":"+port+"/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user. In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+//app.use('/', ensureAuthenticated);
+//app.use('/hello', ensureAuthenticated);
+//app.use('/hello', function (req, res, next) {
+//	console.log('MW ONE');
+//	next();
+//});
+//
+//app.use('/hello', function (req, res, next) {
+//	console.log('MW TWO');
+//	next();
+//});
+
+function ensureAuthenticated(req, res, next) {
+	var ok = req.isAuthenticated();
+	var user = req.query.user;
+	if (user && req.isAuthenticated()) { return next(); }
+	res.redirect('/auth/github');
+}
+
+app.use('/client', ensureAuthenticated);
+app.use(app.router);
+app.use("/client/js/URIjs", express['static'](__dirname + '/node_modules/URIjs/src'));
+app.use("/client", express['static'](__dirname + '/web-editor'));
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+	console.log('Authenticated as '+userName(req));
+	var target = URI(homepage).query({user: userName(req)}).toString();
+	console.log('redirecting: '+target);
+
+	res.redirect(URI(homepage).query({user: userName(req)}).toString());
+    //res.send('Authenticated');
+  }
+);
+
+////////////////////////////////////////////////////////
+// Register http end points
+
+function userDisplayName(req) {
+	return req && req.user && req.user.displayName;
+}
+
+function userName(req) {
+	return req && req.user && req.user.username;
+}
+
+app.get("/",
+	function (req, res) {
+		res.redirect(homepage);
+	}
+);
+
+////////////////////////////////////////////////////////
 
 var messagingHost = process.env.FLIGHT_MESSAGING_HOST || 'localhost';
 var messagingPort = process.env.FLIGHT_MESSAGING_PORT || 3000;
@@ -42,7 +136,7 @@ io.sockets.on('connection', function (socket) {
 // check for MongoDB and create in-memory-repo in case MongoDB is not available
 var MongoClient = mongo.MongoClient;
 MongoClient.connect("mongodb://localhost:27017/flight-db", function(err, db) {
-	
+
 	var Repository;
 	var repository;
 
@@ -54,7 +148,7 @@ MongoClient.connect("mongodb://localhost:27017/flight-db", function(err, db) {
 		console.log('create mongodb-based backup repository');
 		Repository = require('./repository-mongodb.js').Repository;
 	}
-	
+
 	repository = new Repository();
 
 	var RestRepository = require('./repository-rest-api.js').RestRepository;
@@ -68,10 +162,10 @@ MongoClient.connect("mongodb://localhost:27017/flight-db", function(err, db) {
 	var client_socket = client_io.connect(messagingHost, {
 		port : messagingPort
 	});
-	
+
 	client_socket.on('connect', function() {
 		console.log('client socket connected');
-		
+
 		client_socket.emit('connectToChannel', {
 			'channel' : 'internal'
 		}, function(answer) {
---
+++ node.server/web-editor/js/flux-welcome.js
@@ -0,0 +1,168 @@
+/*global $*/
+define(function (require) {
+
+	var socketio = require("editor/socket.io");
+	//Map from projectName -> ProjectView
+	var projects = {};
+
+	var URI = require('URIjs/URI');
+
+	console.log('location = ', window.location);
+
+	var queryString  = window.location.search;
+
+	console.log('parseQuery', URI.parseQuery(queryString));
+
+	var username = URI.parseQuery(queryString).user || 'defaultuser';
+	$(".username").text(username);
+
+
+	//console.log('query=', query);
+
+
+	var socket = io.connect();
+
+	function ProjectView(projectName) {
+		this.name = projectName;
+		this.element = $('<div>').addClass('project');
+		this.element.append(
+			$('<span>').text(projectName).addClass('name')
+		);
+		var oldView = projects[name];
+		projects[projectName] = this;
+		if (oldView) {
+			oldView.dispose();
+		}
+		$("#projectlist").append(this.element);
+		this.resources = {}; //Map resource path to ResourceView
+	}
+	ProjectView.prototype.dispose = function () {
+		$(this.element).remove();
+	};
+	ProjectView.prototype.addResource = function (resource) {
+		console.log('addResource', resource);
+		var type = resource.type;
+		var path = resource.path;
+		//funny resource with path=='' and no type exists. Ignore it
+		if (type==='file' && path) {
+			new ResourceView(this, resource);
+		}
+	};
+	ProjectView.prototype.removeResource = function (path) {
+		var r = this.resources[path];
+		if (r) {
+			r.dispose();
+			delete this.resources[path];
+		}
+	};
+
+	function editorLink(projectName, resource) {
+		//Example:
+		//  http://localhost:3000/client/html/editor.html#defaultuser/Hello-World/src/santa/HoHoHo.java
+		return "editor.html#"+username+"/"+projectName + "/" +resource.path;
+	}
+
+	function ResourceView(parent, resource) {
+		var resources = parent.resources;
+		var old = resources[resource.path];
+		this.element = $('<a>')
+			.text(resource.path)
+			.attr('href', editorLink(parent.name, resource) )
+			.addClass('resource');
+		if (old) {
+			//Insert new element in same place as the old one
+			old.element.after(this.element);
+			old.dispose();
+		} else {
+			parent.element.append(this.element);
+		}
+		resources[resource.path] = this;
+	}
+	ResourceView.prototype.dispose = function () {
+		$(this.element).remove();
+	};
+
+	function projectCreated(projectName) {
+		var projectView = new ProjectView(projectName);
+		socket.emit('getProjectRequest', {
+			'callback_id' : 0,
+			'username' : username,
+			'project' : projectName,
+			'includeDeleted' : false
+		});
+	}
+
+	function projectRemoved(projectName) {
+		var p = projects[projectName];
+		if (p) {
+			p.dispose();
+			delete projects[projectName];
+		}
+	}
+
+	function resourceCreated(username, projectName, file) {
+		var projectView = projects[projectName];
+		if (projectView) {
+			projectView.addResource(file);
+		}
+	}
+
+	function resourceDeleted(username, projectName, path) {
+		var project = projects[projectName];
+		if (project) {
+			project.removeResource(path);
+		}
+	}
+
+	socket.on('connect', function() {
+		if (username) {
+			socket.emit('connectToChannel', {
+				'channel' : username
+			}, function(answer) {
+				socket.emit('getProjectsRequest', {
+					username: username,
+					callback_id: 0
+				});
+
+			});
+		}
+	});
+
+	socket.on('getProjectsResponse', function (data) {
+		console.log('getProjectsResponse', data);
+		var target = $("#projectlist");
+		target.empty();
+		console.log('data', data);
+		console.log('data.projects', data.projects);
+		data.projects.forEach(function (p) {
+			projectCreated(p.name);
+		});
+	});
+
+	socket.on('getProjectResponse', function (data) {
+		console.log('getProjectResponse', data);
+		var username = data.username;
+		var projectName = data.project;
+		data.files.forEach(function (file) {
+			resourceCreated(username, projectName, file);
+		});
+	});
+
+	socket.on('projectConnected', function (data) {
+		projectCreated(data.project);
+	});
+	socket.on('projectDisconnected', function (data) {
+		projectRemoved(data.project);
+	});
+	socket.on('resourceCreated', function (data) {
+		resourceCreated(data.username, data.project, {
+			path: data.resource,
+			type: data.type
+		});
+		console.log('resourceCreated', data);
+	});
+	socket.on('resourceDeleted', function (data) {
+		resourceDeleted(data.username, data.project, data.resource);
+	});
+
+});
