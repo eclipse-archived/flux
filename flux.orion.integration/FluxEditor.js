5236ddc273982d4c71c184161e6f05c77e8025ef
Alex Boyko <aboyko@gopivotal.com>
2014-06-13 15:15:21 -0400

    Problem Markers, Content Assist and live editing Orion -> Eclipse bot the opposite for now

--- flux.orion.integration/FluxEditor.js
+++ flux.orion.integration/FluxEditor.js
@@ -18,6 +18,7 @@ var eclipse = eclipse || {};
 
 var callbacksCache = {};
 var user;
+var muteLiveEdit = false;
 
 var counter = 1;
 function generateCallbackId() {
@@ -41,6 +42,8 @@ eclipse.FluxEditor = (function() {
 			port: port
 		});
 		
+		this._resourceUrl = null;
+		
 		var self = this;
 		
 		this.socket.on('connect', function() {
@@ -54,6 +57,133 @@ eclipse.FluxEditor = (function() {
 					}
 				});
 //			}
+		});
+		
+		this.socket.on('getResourceResponse', function(data) {
+			self._handleMessage(data);				
+		});
+		
+		this.socket.on('contentassistresponse', function(data) {
+			self._handleMessage(data);				
+		});
+
+		this.socket.on('liveResourceStartedResponse', function(data) {
+			self._getResourceData().then(function(resourceMetadata) {
+				if (data.username === resourceMetadata.username 
+					&& data.project === resourceMetadata.project 
+					&& data.resource === resourceMetadata.resource 
+					&& data.callback_id !== undefined
+					&& resourceMetadata.timestamp === data.savePointTimestamp 
+					&& resourceMetadata.hash === data.savePointHash) {
+					muteLiveEdit = true;
+					self._editorContext.setText(data.liveContent).then(function() {
+						muteLiveEdit = false;
+					}, function() {
+						muteLiveEdit = false;
+					});;
+				}
+			}, function(err) {
+				console.log(err);
+			});
+		});
+	
+		this.socket.on('liveResourceStarted', function(data) {
+			self._getResourceData().then(function(resourceMetadata) {
+				if (data.username === resourceMetadata.username 
+					&& data.project === resourceMetadata.project 
+					&& data.resource === resourceMetadata.resource 
+					&& data.callback_id !== undefined
+					&& data.hash === resourceMetadata.hash
+					&& data.timestamp === resourceMetadata.timestamp) {
+		
+					self._editorContext.getText().then(function(contents) {
+						self.sendMessage('liveResourceStartedResponse', {
+							'callback_id'        : data.callback_id,
+							'requestSenderID'    : data.requestSenderID,
+							'username'           : data.username,
+							'project'            : data.project,
+							'resource'           : data.resource,
+							'savePointTimestamp' : resourceMetadata.timestamp,
+							'savePointHash'      : resourceMetadata.hash,
+							'liveContent'        : contents
+						});
+					});
+				}
+			});
+		});
+	
+		this.socket.on('getLiveResourcesRequest', function(data) {
+			self._getResourceData().then(function(resourceMetadata) {
+				if (data.username === resourceMetadata.username 
+					&& data.callback_id !== undefined
+					&& data.project === resourceMetadata.project) {
+						
+					self.sendMessage('getLiveResourcesResponse', {
+						'callback_id'        : data.callback_id,
+						'requestSenderID'    : data.requestSenderID,
+						'liveEditUnits'      : [{
+							'username'           : resourceMetadata.username,
+							'project'            : resourceMetadata.project,
+							'resource'           : resourceMetadata.resource,
+							'savePointTimestamp' : resourceMetadata.timestamp,
+							'savePointHash'      : resourceMetadata.hash
+						}]
+					});
+				}
+			});
+		});
+		
+		this.socket.on('resourceStored', function(data) {
+			var location = self._rootLocation + data.project + '/' + data.resource;
+			if (self._resourceUrl === location) {
+				this._resourceMetadata = data;
+			}
+		});
+		
+		this.socket.on('liveResourceChanged', function(data) {
+			self._getResourceData().then(function(resourceMetadata) {
+				if (data.username === resourceMetadata.username 
+					&& data.project === resourceMetadata.project 
+					&& data.resource === resourceMetadata.resource
+					&& self._editorContext) {
+						
+					var text = data.addedCharacters !== undefined ? data.addedCharacters : "";
+					
+					muteLiveEdit = true;
+					self._editorContext.setText(text, data.offset, data.offset + data.removedCharCount).then(function() {
+						muteLiveEdit = false;
+					}, function() {
+						muteLiveEdit = false;
+					});
+				}
+			});
+		});
+		
+		this.socket.on('liveMetadataChanged', function (data) {
+			self._getResourceData().then(function(resourceMetadata) {
+				if (resourceMetadata.username === data.username 
+					&& resourceMetadata.project === data.project 
+					&& resourceMetadata.resource === data.resource
+					&& data.problems !== undefined) {
+					
+					resourceMetadata.markers = [];
+					var i;
+					for(i = 0; i < data.problems.length; i++) {
+//						var lineOffset = editor.getModel().getLineStart(data.problems[i].line - 1);
+		
+//						console.log(lineOffset);
+		
+						resourceMetadata.markers[i] = {
+							'description' : data.problems[i].description,
+//							'line' : data.problems[i].line,
+							'severity' : data.problems[i].severity,
+							'start' : /*(data.problems[i].start - lineOffset) + 1*/ data.problems[i].start,
+							'end' : /*data.problems[i].end - lineOffset*/ data.problems[i].end
+						};
+					}	
+				}
+				self._handleMessage(data);
+			});			
 		});
 		
 	}
@@ -77,6 +207,8 @@ eclipse.FluxEditor = (function() {
 				if (callbacks) {
 					message.callback_id = generateCallbackId();
 					callbacksCache[message.callback_id] = callbacks;
+				} else {
+					message.callback_id = 0;
 				}
 				this.socket.emit(type, message);
 				return true;
@@ -104,22 +236,168 @@ eclipse.FluxEditor = (function() {
 			return false;
 		},
 		
+		_isFluxResource: function(resourceUrl) {
+			return resourceUrl && resourceUrl.indexOf(this._rootLocation) === 0;
+		},
+		
+		_getResourceData: function() {
+			var request = new orion.Deferred();
+			var self = this;
+			if (self._resourceMetadata) {
+				request.resolve(self._resourceMetadata);
+			} else if (this._resourceUrl) {
+				var normalizedLocation = this._normalizeLocation(this._resourceUrl);
+				this.sendMessage("getResourceRequest", {
+					'username' : user,
+					'project' : normalizedLocation.project,
+					'resource' : normalizedLocation.path,
+					'metadataOnly' : true
+				}, function(data) {
+					var location = self._rootLocation + data.project + '/' + data.resource;
+					if (self._resourceUrl === location) {
+						self._resourceMetadata = data;
+						request.resolve(data);
+					}
+				});
+			} else {
+				request.reject("No resource URL!");
+			}
+			return request;
+		},
+		
+		_setEditorInput: function(resourceUrl, editorContext) {
+			var self = this;
+			if (this._resourceUrl !== resourceUrl) {
+				this._resourceUrl = null;
+				this._editorContext = null;
+				this._resourceMetadata = null;
+				muteLiveEdit = true;
+				if (this._isFluxResource(resourceUrl)) {
+					this._resourceUrl = resourceUrl;
+					this._editorContext = editorContext;
+					muteLiveEdit = false;
+					
+					this._getResourceData().then(function(resourceMetadata) {
+						self.sendMessage('liveResourceStarted', {
+							'callback_id' : 0,
+							'username' : resourceMetadata.username,
+							'project' : resourceMetadata.project,
+							'resource' : resourceMetadata.resource,
+							'hash' : resourceMetadata.hash,
+							'timestamp' : resourceMetadata.timestamp
+						});	
+					});				
+				}
+			}			
+		},
+		
 		onModelChanging: function(evt) {
-			console.log("Editor changing: " + JSON.stringify(evt));					
+			console.log("Editor changing: " + JSON.stringify(evt));
+			var self = this;
+			if (muteLiveEdit) {
+				return;
+			}
+			this._getResourceData().then(function(resourceMetadata) {
+				var changeData = {
+					'username' : resourceMetadata.username,
+					'project' : resourceMetadata.project,
+					'resource' : resourceMetadata.resource,
+					'offset' : evt.start,
+					'removedCharCount' : evt.removedCharCount,
+					'addedCharacters' : evt.text,
+				};
+		
+				self.sendMessage('liveResourceChanged', changeData);
+			});
 		},
 		
 		computeContentAssist: function(editorContext, options) {
-			console.log("Editor content assist: " + JSON.stringify(editorContext));
+			var request = new orion.Deferred();
+			var self = this;
+			this._getResourceData().then(function(resourceMetadata) {
+				self.sendMessage("contentassistrequest", {
+					'username' : resourceMetadata.username,
+					'project' : resourceMetadata.project,
+					'resource' : resourceMetadata.resource,
+					'offset' : options.offset,
+					'prefix' : options.prefix,
+					'selection' : options.selection
+				}, function(data) {
+					var proposals = [];
+					if (data.proposals) {
+						data.proposals.forEach(function(proposal) {
+							var name;
+							var description;
+							if (proposal.description 
+								&& proposal.description.segments 
+								&& (Array.isArray && Array.isArray(proposal.description.segments) || proposal.description.segments instanceof Array)) {
+								
+								if (proposal.description.segments.length >= 2) {
+									name = proposal.description.segments[0].value;
+									description = proposal.description.segments[1].value;
+								} else {
+									description = proposal.description.segments[0].value;
+								}
+							} else {
+								description = proposal.description;
+							}
+							if (!description) {
+								description = proposal.proposal;
+							}
+							if (description) {
+								proposals.push({
+									'description' : description,
+									'name' : name,
+									'overwrite' : proposal.replace,
+									'positions' : proposal.positions,
+									'proposal' : proposal.proposal,
+									'style' : "emphasis",
+									'escapePosition' : proposal.escapePosition
+								});
+							}
+						});
+					}
+					console.log("Editor content assist: " + JSON.stringify(proposals));
+					request.resolve(proposals);
+				});
+			});
+			return request;
 		},
 		
 		computeProblems: function(editorContext, options) {
-			console.log("Validator (Problems): " + JSON.stringify(options));	
+			console.log("Validator (Problems): " + JSON.stringify(options));
+			var problemsRequest = new orion.Deferred();
+			this._setEditorInput(options.title, editorContext);
+			this._waitForProblemMarkers(this._resourceUrl, 50, function(markers) {
+				problemsRequest.resolve(markers);
+			}, function() {
+				problemsRequest.reject();
+			});			
+			return problemsRequest;
 		},
 		
-		checkSyntax: function(title, contents) {
-			console.log("Validator (Syntax): " + JSON.stringify(title));	
+		_waitForProblemMarkers: function(resourceUrl, interval, successCallback, failureCallback) {
+			var self = this;			
+			var wait = function() {
+				self._getResourceData().then(function(resourceMetadata) {
+					if (self._resourceUrl === resourceUrl) {
+						if (resourceMetadata.markers) {
+							if (successCallback && successCallback.call) {
+								successCallback.call(self, resourceMetadata.markers);
+							}
+						} else {
+							setTimeout(wait, interval);
+						}
+					} else {
+						if (failureCallback && failureCallback.call) {
+							failureCallback.call(self);
+						}
+					}	
+				});
+			};
+			wait.call(this);
 		},
-
+		
 	};
 
 	return FluxEditor;
