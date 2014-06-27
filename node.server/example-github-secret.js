2f0d92debbb00b5b170a35678ad93779d73fa22e
Kris De Volder <kdevolder@gopivotal.com>
2014-06-27 14:42:47 -0700

    Example github secret

---
+++ node.server/example-github-secret.js
@@ -0,0 +1,5 @@
+// Get your app id and secret from
+// https://github.com/settings/applications
+// then rename this file to 'github-secret.js'
+exports.id = 'GET YOUR OWN';
+exports.secret = 'GET YOUR OWN';
