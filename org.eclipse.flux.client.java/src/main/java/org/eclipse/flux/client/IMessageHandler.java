7f18a84f06c8dea7a4ca308d4a345d36ceffb3ec
Fjodor Vershinin <fjodor@vershinin.net>
2016-08-21 08:06:09 +0300

    Added static fields in order to store message types
    Signed-off-by: Fjodor Vershinin <fjodor@vershinin.net>

--- org.eclipse.flux.client.java/src/main/java/org/eclipse/flux/client/IMessageHandler.java
+++ org.eclipse.flux.client.java/src/main/java/org/eclipse/flux/client/IMessageHandler.java
@@ -19,6 +19,22 @@ import org.json.JSONObject;
  *
  */
 public interface IMessageHandler {
+    public static final String GET_PROJECT_REQUEST = "getProjectRequest";
+    public static final String GET_PROJECT_RESPONSE = "getProjectResponse";
+    public static final String GET_LIVE_RESOURCE_REQUEST = "getLiveResourcesRequest";
+    public static final String GET_METADATA_REQUEST = "getMetadataRequest";
+    public static final String GET_METADATA_RESPONSE = "getMetadataResponse";
+    public static final String GET_RESOURCE_REQUEST = "getResourceRequest";
+    public static final String GET_RESOURCE_RESPONSE = "getResourceResponse";
+    public static final String LIVE_RESOURCE_STARTED = "liveResourceStarted";
+    public static final String LIVE_RESOURCE_STARTED_RESPONSE = "liveResourceStartedResponse";
+    public static final String LIVE_RESOURCE_CHANGED = "liveResourceChanged";
+    public static final String PROJECT_CONNECTED = "projectConnected";
+    public static final String PROJECT_DISCONNECTED = "projectDisconnected";
+    public static final String RESOURCE_CHANGED = "resourceChanged";
+    public static final String RESOURCE_CREATED = "resourceCreated";
+    public static final String RESOURCE_DELETED = "resourceDeleted";
+    public static final String RESOURCE_STORED = "resourceStored";
 	
 	boolean canHandle(String type, JSONObject message);
 	
